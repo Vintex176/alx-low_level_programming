f43ad024281b90c5ed8b13595ca9eae8e26ae94a
Vintex176 <vinnyvintex@gmail.com>
2022-07-20 14:06:06 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,16 @@
-#include "main.h"
-
+include "main.h"
 /**
- * _print_rev_recursion - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ *
+ *_print_rev_recursion - prints a string in reverse
+ *@s: pointer block of memory to fill
+ *Return: void
  */
+
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-		_print_rev_recursion(s + 1);
-	else
-		return;
-	_putchar(*s);
+	if (*s) /*If statement*/
+	{
+		_print_rev_recursion(s + 1); /*add s*/
+		_putchar(*s); /*print s*/
+	}
 }
