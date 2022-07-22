f8cdfaeeb3c422c16db5144bda8faaa40fd0d867
Vintex176 <vinnyvintex@gmail.com>
2022-07-22 05:01:04 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,15 +1,6 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - function
- * @argc: offset count
- * @argv: offset value
- * Return: 0
- */
+int _putchar(char c);
 
-
-int main(int argc, char *argv[])
-{
-	printf("%s\n", argv[argc - 1]);
-	return (0);
-}
+#endif /* MAIN_H */
